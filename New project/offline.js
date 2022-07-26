5b4026845bf83efaab43230cced058873a859a0b
Triate <72053415+Triate@users.noreply.github.com>
2022-07-26 18:58:36 -0300

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1658871860,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/e264858b8f8c18db1791930230b0ba-sheet0.png",
+		"images/download-sheet0.png",
+		"images/download-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
